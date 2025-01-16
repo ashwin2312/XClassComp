c42e92ef5e3557223597a847b5a6c3766caa2447
Ashwin <ashvinkumar8891@gmail.com>
2025-01-16 22:05:02 +0530

    class based counter

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count} </p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
